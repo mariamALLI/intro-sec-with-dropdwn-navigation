5cf2982f3db0dc0ed8dd1778c90f73914cef1aee
Mariam O-Alli <81536650+mariamALLI@users.noreply.github.com>
2022-11-22 11:20:16 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const dropDown1 = document.querySelector(".dropdown_menu");
+const dropDown2 =  document.querySelector(".dropdown-menu");
+const arrowToggle1 = document.querySelector(".arrowicon1");
+const arrowToggle2 = document.querySelector(".arrowicon2");
+
+const sideMenu = document.querySelector('#sidemenu')
+const openMenu = document.getElementById("open_menu")
+
+openMenu.addEventListener('click', function(){
+    document.querySelector('#sidemenu').style.right = '0'
+    })
+
+function closeMenu(){
+    sideMenu.style.right = "-240px"
+}
+
+arrowToggle1.addEventListener('click', () => {
+    const expand = dropDown1.getAttribute("data-visible");
+
+    if(expand === "false") {
+        dropDown1.setAttribute("data-visible", true)
+        arrowToggle1.setAttribute("aria-expanded", true)
+    }else if (expand === "true") {
+        dropDown1.setAttribute("data-visible", false)
+        arrowToggle1.setAttribute("aria-expanded", false)
+    }
+})
+
+arrowToggle2.addEventListener('click', () => {
+    const expand2 = dropDown2.getAttribute("data-visible");
+
+    if(expand2 === "false"){
+        dropDown2.setAttribute("data-visible", true)
+        arrowToggle2.setAttribute("aria-expanded", true)
+    }else if(expand2 === "true"){
+        dropDown2.setAttribute("data-visible", false)
+        arrowToggle2.setAttribute("aria-expanded", false)
+    }
+})
+
